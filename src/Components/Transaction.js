4f25445359516b11620d8b0336ca3b87082da0db
vikash keswani <vikashkeswani@gmail.com>
2020-07-02 08:47:00 -0700

    expense tracker app completed

---
+++ src/Components/Transaction.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react' ;
+import GlobalContext from '../context/GlobalState' ;
+
+
+export const Transaction = (props) => {
+    const {deleteTransaction} = useContext(GlobalContext) ;
+    
+    const sign = props.transaction.amount < 0 ? '-' : '+' ;
+    return(
+        <li className={props.transaction.amount < 0 ? 'minus' : 'plus' }>
+             {props.transaction.text} <span>{sign}${Math.abs(props.transaction.amount)}</span>
+             <button onClick={() => deleteTransaction(props.transaction.id)} className="delete-btn">X</button>
+        </li>
+    )
+}
+export default Transaction ;
